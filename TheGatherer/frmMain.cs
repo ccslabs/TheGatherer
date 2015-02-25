8ac4c0a76e745ccee80e39601b15c6dfc3f17941
ccslabs <ccslabs@hotmail.com>
2015-02-25 17:59:02 +0000

    GetURLToProcess()

--- TheGatherer/frmMain.cs
+++ TheGatherer/frmMain.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,76 @@ namespace TheGatherer
 {
     public partial class frmMain : Form
     {
+
+        private double seconds = 0;
+        private double previousSeconds = 0;
+
+        private bool PauseOperations = false;
+
         public frmMain()
         {
             InitializeComponent();
         }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            previousSeconds = Properties.Settings.Default.PreviousRuntime;
+            lblVersion.Text = CoreAssembly.Version.ToString();
+
+           
+        }
+
+        private void RunThread()
+        {
+            while(!PauseOperations)
+            {
+                string CurrentURL = GetURLToProcess(); // Get Next URL from the Database
+            }
+        }
+
+        private string GetURLToProcess()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        private void timerOneSecond_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            TimeSpan thisSessionRuntime = TimeSpan.FromSeconds(seconds);
+            TimeSpan totalRuntime = TimeSpan.FromSeconds(seconds + previousSeconds);
+
+            lblRunTimeThisSession.Text = thisSessionRuntime.ToString();
+            lblRunTimeTotal.Text = totalRuntime.ToString();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.PreviousRuntime = previousSeconds + seconds;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnStartPause_Click(object sender, EventArgs e)
+        {
+            if(btnStartPause.Text == "Start")
+            {
+                PauseOperations = false;
+                btnStartPause.Text = "Pause";
+                // kick the thread into action
+                Task.Run(() => RunThread());
+            }
+            else
+            {
+                btnStartPause.Text = "Start";
+                PauseOperations = true;
+            }
+        }
+    }
+
+    public static class CoreAssembly
+    {
+        public static readonly Assembly Reference = typeof(CoreAssembly).Assembly;
+        public static readonly Version Version = Reference.GetName().Version;
     }
 }
